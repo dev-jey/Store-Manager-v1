caad4942d316b2db201be26f1496ac1ea2ee28e9
Jey <savalijey@gmail.com>
2018-10-18 19:16:07 +0300

    [#161308487]Create file structure and make initial environment organization

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, Blueprint
+from instance.config import app_config
+from .api.v1 import blprint as version1
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('../instance/config.py')
+    app.register_blueprint(version1)
+    return app
