b188765884fb3797b679d5b0cbee71f78588bca3
Jey <savalijey@gmail.com>
2018-10-18 20:18:11 +0300

    [#161309528] Create failing tests for login module

--- app/test/v1/test_api.py
+++ app/test/v1/test_api.py
@@ -3,6 +3,7 @@ import json
 
 from app import create_app
 from instance.config import app_config
+from app.api.v1.models import destroy
 
 
 class TestsForApi(unittest.TestCase):
@@ -15,29 +16,49 @@ class TestsForApi(unittest.TestCase):
                         "password": "as@dsDdz2a",
                         "role": "Admin"
                         })
-
+        self.admin_login_details = json.dumps({
+            "email": "maria@gmail.com",
+            "password": "as@dsDdz2a"
+            })
         signup_admin = self.test_client.post("/api/v1/auth/signup",
                                              data=self.admin_info,
                                              headers={
                                               'content-type': 'application/json'
                                              })
+        admin_login = self.test_client.post("/api/v1/auth/login",
+                                            data=self.admin_login_details,
+                                            headers={
+                                             'content-type': 'application/json'
+                                            })
+        self.admin_token = json.loads(admin_login.data.decode())["token"]
         self.attendant = json.dumps({
                         "id": 2,
                         "email": "james@gmail.com",
                         "password": "as@dsDdz2a",
                         "role": "Attendant"
                         })
-        
+        self.attendant_login_details = json.dumps({
+                                                 "email": "james@gmail.com",
+                                                 "password": "as@dsDdz2a"
+                                                 })
         signup_attendant = self.test_client.post("/api/v1/auth/signup",
                                                  data=self.attendant,
                                                  headers={
                                                   'content-type': 'application/json'
                                                  })
 
+        login_attendant = self.test_client.post("/api/v1/auth/login",
+                                                data=self.attendant_login_details,
+                                                headers={
+                                                 'content-type': 'application/json'
+                                                })
+        self.data = json.loads(login_attendant.data.decode())
+        self.attendant_token = self.data["token"]
         self.context = self.app.app_context()
         self.context.push()
 
     def tearDown(self):
+        destroy()
         self.context.pop()
 
     def test_successful_signup(self):
@@ -52,6 +73,32 @@ class TestsForApi(unittest.TestCase):
                                             'content-type': 'application/json'
                                             })
         self.assertEqual(res.status_code, 201)
+
+    def test_success_login(self):
+        resp = self.test_client.post("/api/v1/auth/login",
+                                     data=self.admin_login_details,
+                                     headers={
+                                        'content-type': 'application/json'
+                                            })
+        self.assertEqual(resp.status_code, 200)
+
+    def test_fail_login(self):
+        response = self.test_client.post("/api/v1/auth/login",
+                                         data=json.dumps({
+                                                        "email": "m@ew",
+                                                        "password": "dxfdfd#eE"
+                                                        }),
+                                         headers={
+                                            'content-type': 'application/json'
+                                                })
+        self.assertEqual(response.status_code, 403)
+
+    def test_login_with_no_credentials_given(self):
+        resp = self.test_client.post("/api/v1/auth/login",
+                                     headers={
+                                             'content-type': 'application/json'
+                                            })
+        self.assertEqual(resp.status_code, 400)
 
     def test_wrong_email_signup(self):
         resp = self.test_client.post("/api/v1/auth/signup",
