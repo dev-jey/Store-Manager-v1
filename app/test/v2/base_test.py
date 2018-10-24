c6930e27d3744df048c20318a90dfec36ef97925
Jey <savalijey@gmail.com>
2018-10-24 18:26:40 +0300

    [Feature #161420737] Add data to test setup

--- app/test/v2/base_test.py
+++ app/test/v2/base_test.py
@@ -34,6 +34,28 @@ class TestsForApi(unittest.TestCase):
                                                 'content-type': 'application/json'
                                             })
         self.admin_token = json.loads(admin_login.data.decode())["token"]
+        self.attendant = json.dumps({
+            "email": "james@gmail.com",
+            "password": "as@dsDdz2a",
+                        "role": "Attendant"
+        })
+        self.attendant_login_details = json.dumps({
+            "email": "james@gmail.com",
+            "password": "as@dsDdz2a"
+        })
+        signup_attendant = self.test_client.post("/api/v2/auth/signup",
+                                                 data=self.attendant,
+                                                 headers={
+                                                     'content-type': 'application/json'
+                                                 })
+
+        login_attendant = self.test_client.post("/api/v2/auth/login",
+                                                data=self.attendant_login_details,
+                                                headers={
+                                                    'content-type': 'application/json'
+                                                })
+        self.data = json.loads(login_attendant.data.decode())
+        self.attendant_token = self.data["token"]
         self.context = self.app.app_context()
         self.context.push()
 
