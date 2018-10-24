d1d4c74edcdb11c41ce12fad6db5ee03b0f66553
Jey <savalijey@gmail.com>
2018-10-24 03:11:46 +0300

    [Feature #161420591] Create failing tests for signup

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 
-from .views import SignUp, Login, Product, Sale, OneProduct, OneSale
+from .views import SignUp
 
 '''Creation of blue prints and routes for endpoints'''
 blprint2 = Blueprint('api', __name__, url_prefix="/api/v2")
---
+++ app/test/v2/base_test.py
@@ -0,0 +1,24 @@
+import unittest
+import json
+
+from app import create_app
+from instance.config import app_config
+from app.api.v2.models.db_models import Db
+
+
+class TestsForApi(unittest.TestCase):
+    '''Set up method to create an attendant, admin, product,
+    and a sales table for use in other tests and authentication'''
+    def setUp(self):
+        self.db = Db()
+        self.db.createTables()
+        self.app = create_app(config_name="testing")
+        self.test_client = self.app.test_client()
+        self.context = self.app.app_context()
+        self.context.push()
+
+    def tearDown(self):
+        '''Method to clear all tables
+        before another test is undertaken'''
+        self.db.destroy_tables()
+        self.context.pop()
