505032b5ae12cd747a7c5f547145efc5c52a09b7
Jey <savalijey@gmail.com>
2018-10-26 12:02:58 +0300

    [Bug #161493704] Fix Heroku deploy

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from .api.v2 import blprint2 as version2
 def create_app(config_name):
     '''Main function to create the flask app and
     set configurations'''
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('../instance/config.py')
     app.register_blueprint(version2)
