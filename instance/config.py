83f40fdf2f6ed46c7af910e8bb5235b25498e2e4
Jey <savalijey@gmail.com>
2018-11-01 13:37:14 +0300

    [Bug #161597697] Rectify config.py

--- instance/config.py
+++ instance/config.py
@@ -19,6 +19,11 @@ class Development(Config):
 
 class Testing(Config):
     '''Class to set Testing mode configurations'''
+    DB_NAME = os.getenv("TEST_DB_NAME")
+    DB_HOST = os.getenv("TEST_DB_HOST")
+    DB_USER = os.getenv("TEST_DB_USER")
+    DB_PASSWORD = os.getenv("TEST_DB_PASSWORD")
+    APP_SETTINGS = "testing"
     DEBUG = False
 
 '''A dictionary to store all configurations for easy access'''
